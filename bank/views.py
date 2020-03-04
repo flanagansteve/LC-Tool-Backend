3c504a6b5f050778de4c389e470edc611b285af8
Steve Flanagan <flanagan.s@husky.neu.edu>
2020-03-04 12:49:03 -0500

    upgraded all JSON responses to use djangos built-in JsonResponse

--- bank/views.py
+++ bank/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseBadRequest, Http404
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest, Http404
 from django.core import serializers
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
 from .models import Bank, BankEmployee
 import json, datetime
 
@@ -16,8 +17,7 @@ def index(request):
     # TODO is there ever a situation where we GET all the banks?
     if request.method == "GET":
         all_banks = Bank.objects.all()
-        all_banks_json = serializers.serialize('json', all_banks)
-        return HttpResponse(all_banks_json, content_type="application/json")
+        return JsonResponse(list(all_banks.values()), safe=False)
     elif request.method == "POST":
         json_data = json.loads(request.body)
         # 1. create the bank
@@ -40,8 +40,13 @@ def index(request):
         login(request, first_user)
         # 4. return the objects_created (user object, bank) as well as a session obj
         now = str(datetime.datetime.now())
-        return HttpResponse("{\"bountium_access_token\":\"" + first_user.username + now + "\"," +
-            "\"objects_created\":" + serializers.serialize('json', [ bank, first_user, ]) + "}")
+        response = {
+            "bountium_access_token" : first_user.username + now,
+            "objects_created" : [
+                model_to_dict(bank), model_to_dict(first_user)
+            ]
+        }
+        return JsonResponse(response)
     else:
         raise HttpResponseBadRequest("This endpoint only supports GET, POST")
 
@@ -53,7 +58,7 @@ def rud_bank(request, bank_id):
             bank = Bank.objects.get(id=bank_id)
         except Bank.DoesNotExist:
             raise Http404("No bank with id " + bank_id)
-        return HttpResponse(serializers.serialize('json', [ bank, ]), content_type="application/json")
+        return JsonResponse(model_to_dict(bank))
     elif request.method == "DELETE":
         try:
             Bank.objects.delete(bank_id)
@@ -76,81 +81,84 @@ def rud_bank(request, bank_id):
 # TODO authenticate this - whos allowed to invite teammates?
 @csrf_exempt
 def invite_teammate(request, bank_id):
-    try:
-        bank = Bank.objects.get(id=bank_id)
-    except Bank.DoesNotExist:
-        raise Http404("No bank with id " + bank_id + " for you to invite a teammate to")
-    # TODO handle empty body, bad json, or non-post-request
-    json_data = json.loads(request.body)
-    # 1a. Has this teammate hasn't already been invited?
-    # TODO handle key error
-    invitee_email = json_data['invitee_email']
-    try:
-        invitee = bank.bankemployee_set.get(email = invitee_email)
-        # 2. if so - have they registered?
-        if invitee.name is not None:
-            # 2a. if they have - return status:registered and the user object
-            return HttpResponse("{\"status\":\"registered\", \"employee\":" + serializers.serialize(invitee) + "}", content_type="application/json")
-        else:
-            # 2c. if they have not - re-invite, then return status:reinvited [now]
+    if request.method == "POST":
+        try:
+            bank = Bank.objects.get(id=bank_id)
+        except Bank.DoesNotExist:
+            raise Http404("No bank with id " + bank_id + " for you to invite a teammate to")
+        # TODO handle empty body, bad json, or non-post-request
+        json_data = json.loads(request.body)
+        response = {"status" : "registered"}
+        # 1a. Has this teammate already been invited?
+        try:
+            invitee_email = json_data['invitee_email']
+        except KeyError:
+            return HttpResponseBadRequest("You must send a request with a JSON object body, with an \"invitee_email\" field")
+        try:
+            invitee = bank.bankemployee_set.get(email = invitee_email)
+            # 2. if so - have they registered?
+            if invitee.name is not None:
+                # 2a. if they have - return status:registered and the user object
+                response["employee"] = model_to_dict(invitee)
+            else:
+                # 2c. if they have not - re-invite, then return status:reinvited [now]
+                # TODO write the email to send as args: subject, message, from_email=None
+                User.objects.get(email = invitee_email).email_user()
+                now = str(datetime.datetime.now())
+                response["status"] = "re-invited on " + now
+        # 1b. If they have not been invited
+        except BankEmployee.DoesNotExist:
+            # 2. mail an invite
             # TODO write the email to send as args: subject, message, from_email=None
             User.objects.get(email = invitee_email).email_user()
+            # 3. save them and return status:invited [now]
+            bank.bankemployee_set.create(email = invitee_email)
             now = str(datetime.datetime.now())
-            return HttpResponse("{\"status\":\"re-invited on " + now + "\"}", content_type="application/json")
-    # 1b. If they have not been invited
-    except BankEmployee.DoesNotExist:
-        # TODO 2. mail an invite
-        # TODO write the email to send as args: subject, message, from_email=None
-        User.objects.get(email = invitee_email).email_user()
-        # 3. save them and return status:invited [now]
-        # TODO exceptions to handle here?
-        bank.bankemployee_set.create(email = invitee_email)
-        now = str(datetime.datetime.now())
-        return HttpResponse("{\"status\":\"invited on \"" + now + "\"}", content_type="application/json")
+            response["status"] = "invited on " + now
+        return JsonResponse(response)
+    else:
+        return HttpResponseBadRequest("This endpoint only supports POST")
 
 # POST
 # - email, both to validate the invite & create credentials
 # - password
 # - name and title (NOTE we might make this optional)
 # and receive back
-# - a user object w/ session cookie
-# TODO handle key errors
+# - TODO what does ryan want back?
 @csrf_exempt
 def register_upon_invitation(request, bank_id):
-    new_user_data = json.loads(request.body)
-    # 1. First, verify that this user has indeed been invited to the bank
-    # they're trying to register into
-    # a. get the bank by bank_id. Check for error
-    try:
-        bank = Banks.objects.get(id=bank_id)
-    except Bank.DoesNotExist:
-        return Http404("There is no bank with id " + bank_id)
-    # b. check if there is a bankemployee with email=new_user_data['email'],
-    #    and blanks for all other fields. Check for error on either.
-    new_employee = Bank.bankemployee_set.get(email=new_user_data['emai'])
-    if new_employee is None:
-        return Http404("There is no invitation for email " + new_user_data['emai'])
-    if new_employee.username:
-        return HttpResponse("Someone has already used this invitation. Ask whoever administers Bountium at your employer about this.", status=401)
-    # 2. Register the user account
-    new_user = User.objects.create_user(username=new_user_data['email'],
-                             email=new_user_data['email'],
-                             password=new_user_data['password'])
-    new_user = authenticate(username=new_user_data['email'], password=new_user_data['password'])
-    login(request, new_user)
-    # 3. Update the bankemployee with full fields
-    bank.bankemployee_set.update(new_employee.id,
-        name = new_user_data['name'],
-        title = new_user_data['title'])
-    # 4. return user object w/token
-    request.session['logged_in'] = True
-    # TODO what does ryan need to know after a login?
-        # does he need a special cookie?
-        # does he need params about the user, like which bank they're a part of?
-            # or will he ask for those other things as he needs them?
-    # using this for testing:
-    now = str(datetime.datetime.now())
-    return HttpResponse('{\"bountium_access_token\":\"' + new_user.username + now + "\"}", content_type="application/json")
+    if request.method == "POST":
+        new_user_data = json.loads(request.body)
+        # 1. First, verify that this user has indeed been invited to the bank
+        # they're trying to register into
+        # a. get the bank by bank_id. Check for error
+        try:
+            bank = Bank.objects.get(id=bank_id)
+        except Bank.DoesNotExist:
+            return Http404("There is no bank with id " + bank_id)
+        # b. check if there is a bankemployee with email=new_user_data['email'],
+        #    and blanks for all other fields. Check for error on either.
+        new_employee = bank.bankemployee_set.get(email=new_user_data['email'])
+        if new_employee is None:
+            return Http404("There is no invitation for email " + new_user_data['email'])
+        if new_employee.username:
+            return HttpResponse("Someone has already used this invitation. Ask whoever administers Bountium at your employer about this.", status=401)
+        # 2. Register the user account
+        new_user = User.objects.create_user(username=new_user_data['email'],
+                                 email=new_user_data['email'],
+                                 password=new_user_data['password'])
+        new_user = authenticate(username=new_user_data['email'], password=new_user_data['password'])
+        login(request, new_user)
+        # 3. Update the bankemployee with full fields
+        bank.bankemployee_set.update(new_employee.id,
+            name = new_user_data['name'],
+            title = new_user_data['title'])
+        # 4. return user object w/token
+        # using this for testing:
+        now = str(datetime.datetime.now())
+        return HttpResponse('{\"bountium_access_token\":\"' + new_user.username + now + "\"}", content_type="application/json")
+    else:
+        return HttpResponseBadRequest("This endpoint only accepts POST requests")
 
 # TODO authenticate this - whos allowed to R (and in what detail), and to UD?
 def rud_bank_employee(request, bank_id, employee_id):
@@ -161,7 +169,7 @@ def rud_bank_employee(request, bank_id, employee_id):
     # lol python has no switch(). could use a dict + lambdas, but its only 3 branches...
     if request.method == "GET":
         try:
-            return HttpResponse(serializers.serialize('json', [ bank.bankemployee_set.get(id=employee_id), ]), content_type="application/json")
+            return JsonResponse(model_to_dict(bank.bankemployee_set.get(id=employee_id)))
         except BankEmployee.DoesNotExist:
             raise Http404(str(bank) + " does not have an employee with id " + employee_id)
     elif request.method == "DELETE":
--- user/views.py
+++ user/views.py
@@ -1,14 +1,13 @@
-from django.http import HttpResponse, HttpResponseBadRequest, Http404, HttpResponseForbidden
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest, Http404, HttpResponseForbidden
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 import json, datetime
 
 # POST email & password, receive back one of
-# - a user object w/ session cookie
+# - TODO return the User, the user's employee object, & the user's employer object
 # - a rejection for invalid creds
 # TODO upgrade to django-oauth-toolkit
-# TODO return the User, the user's employee object, & the user's employer object
 @csrf_exempt
 def user_login(request):
     if request.method == "POST":
@@ -21,7 +20,7 @@ def user_login(request):
         if user is not None:
             login(request, user)
             now = str(datetime.datetime.now())
-            return HttpResponse('{\"bountium_access_token\":\"' + user.username + now + "\"}", content_type="application/json")
+            return JsonResponse({"bountium_access_token" : user.username + now})
         else:
             return HttpResponseForbidden('Invalid credentials')
     else:
@@ -33,7 +32,7 @@ def user_logout(request):
     if request.method == "POST":
         if request.user.is_authenticated():
             logout(request, user)
-            return HttpResponse("{\"success\":true}")
+            return JsonResponse({"success":True})
         else:
             return HttpResponseForbidden('You must be logged in to log out')
     else:
