d3f63a2d88c86e0b80585a3f242e11c31f2736c1
Steve Flanagan <flanagan.s@husky.neu.edu>
2020-03-08 16:54:18 -0400

    massive modeling progress + defining endpoint functionality

--- lc/models.py
+++ lc/models.py
@@ -1,19 +1,16 @@
 from django.db import models
-from business.models import Business
-from bank.models import Bank, BankEmployee
+from bank.models import Bank, BankEmployee, DigitalLCApplication
 from business.models import Business, BusinessEmployee
 
+# Abstract LC from which Pdf and Digital inherit
+# TODO add assigning employees to an LC
 class LC(models.Model):
-    # TODO should these cascade on delete or not?
+    # -- the parties to an LC --
     client = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='client')
     beneficiary = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='beneficiary')
     issuer = models.ForeignKey(Bank, on_delete=models.CASCADE)
-    # CDM can be ('Courier', 'SWIFT') or other
-    credit_delivery_means = models.CharField(max_length=250)
-    # Goes up to 1,000T
-    credit_amt = models.DecimalField(max_digits=17, decimal_places=2)
-    # TODO this should technically be an enum
-    currency_denomination = models.CharField(max_length=5, default='USD')
+
+    # -- the status of an LC -- #
     # TODO do terms_satisfied & paid_out fields make sense for a Standby LC?
     # theoretically it could be 'satisfied' and 'pay out more than once' -
     # the difference is, of course, that satisfaction is a bad thing
@@ -24,11 +21,44 @@ class LC(models.Model):
         # about LCs, it may be counterproductive. This thinking
         # could seep into the UX even if its only meant to be
         # an elegant shortcut on the back end. Idk!
+    issuer_approved = models.BooleanField()
+    beneficiary_approved = models.BooleanField()
     terms_satisfied = models.BooleanField()
+    requested = models.BooleanField()
+    drawn = models.BooleanField()
     paid_out = models.BooleanField()
+
+class PdfLC(LC):
+    application = models.FileField(upload_to=str(self.issuer) + '/' + str(self.client) + '/applications/%Y/%m/%d')
+    contract = models.FileField(upload_to=str(self.issuer) + '/' + str(self.client) + '/lcs/%Y/%m/%d')
+
+class DigitalLC(LC):
+    # TODO should this cascade on delete or not?
+    for_app = models.ForeignKey(DigitalLCApplication, on_delete=models.CASCADE)
+    # TODO what should the max length of appResponseAsJSON be?
+    # We store an application response as JSON array of objects; the format is
+    """
+    {
+        'for_question': the index of the question in the corresponding DigitalLCApplication's array of questions,
+        'answer_value': the answer's value, as an int or double or text or w/e
+    }
+    """
+    # However, we also try to extract as many fields into actual data as possible
+    app_response_as_JSON = models.CharField(max_length=1000)
+
+    # -- the actual data of this lc -- #
+    # TODO add all the other fields in the SVB template
+    # CDM can be ('Courier', 'SWIFT') or other
+    credit_delivery_means = models.CharField(max_length=250)
+    # Goes up to 1,000T
+    credit_amt = models.DecimalField(max_digits=17, decimal_places=2)
+    # TODO this should technically be an enum
+    currency_denomination = models.CharField(max_length=5, default='USD')
     # TODO this should technically be an enum, one of
     # ['Commercial', 'Standby', 'Import', 'Export']
-    currency_denomination = models.CharField(max_length=20, default='Commercial')
+    type = models.CharField(max_length=20, default='Commercial')
+
+    # TODO someday: def to_pdf()
 
 class DocumentaryRequirement(models.Model):
     for_lc = models.ForeignKey(LC, on_delete=models.CASCADE)
@@ -43,6 +73,6 @@ class DocumentaryRequirement(models.Model):
     # for this value
     required_values = models.CharField(max_length=500)
     due_date = models.DateField()
-    # TODO how to store a document file?
-    #submitted_doc = models.
+    link_to_submitted_doc = models.CharField(max_length=250, blank=True)
+    complaints = models.CharField(max_length=1000)
     satisfied = models.BooleanField()
--- lc/urls.py
+++ lc/urls.py
@@ -4,20 +4,125 @@ from . import views
 """ Endpoints:
 1. /lc/
 # GET all the lcs (TODO: thats probably just for testing)
-# POST (TODO: who gets to POST these?) the following to create an LC
+# POST the following to create an LC
+    # If its a BusinessEmployee POSTing, we expect one of
+    [aFilledOutPDFApp.pdf]
+    or
+    {
+        [the fields of a DigitalLC]
+    }
+    # and receive back
+    {
+        'success' : true || false,
+        'lc_id' : an integer of the ID of your lc
+    }
+    # If its a BankEmployee POSTing, we expect
+    [app_response_that_i_uploaded_on_behalf_of_a_client.pdf]
+    or, for creating an LC app that your client will fill out later (which bountium will notify them about), one of
+    {
+        'applicant_id' : [an int if this is a repeat LC applicant that the form autofilled]
+    }
+    {
+        'applicant' : 'the applicants business name',
+        'applicant_employee_contact' : 'someemployee@business.com'
+        [we'll create the business in our database in this case]
+    }
+
+2. /lc/{lc_id} (a very busy endpoint lol)
+# PUT to update an LC
+# TODO this handles redlining - how are we going to save the history of redlined changes to render back to the user?
+    # If its a BusinessEmployee PUTing...
+        # If
+            !(<lc for which id == lc_id>.filledOut)
+            && (<the employee PUTing>.employer == <lc for which id == lc_id>.applicant)
+        we expect this to be the rest of the values required to create an LC - one of:
+        [aFilledOutPDFApp.pdf]
+        or
+        {
+            [the fields of a digital LC model]
+        }
+        # If
+            !(<lc for which id == lc_id>.beneficiaryApproved)
+            && (<the employee PUTing>.employer == <lc for which id == lc_id>.beneficiary)
+        we expect this to be part of the redlining process, and thus, expect one of:
+        [beneficiary_redlined_contract.pdf]
+        or
+        {
+            [the updated fields of a digital LC model]
+        }
+        We will:
+        1. update the LC to the version we just received
+        2. mark it as beneficiary_approved = True
+        3. notify the issuing bank and await their approval
+    # If its a BankEmployee PUTing...
+        # If
+            (<lc for which id == lc_id>.approved)
+            && (<the employee PUTing>.bank == <lc for which id == lc_id>.issuer)
+        we expect this to be part of the redlining process, and thus, expect one of:
+        [issuer_redlined_contract.pdf]
+        or
+        {
+            [the updated fields of a digital LC model]
+        }
+        We will:
+        1. update the LC to the version we just received
+        2. mark it as issuer_approved = True
+        3. notify the counterparty and await their approval
+# GET, and receive back
 {
-    TODO
+    [the fields of a DigitalLC]
 }
-# and receive back
+or
 {
-    TODO
+    [the fields of a PdfLC]
 }
 
-2. /lc/{lc_id}
-# GET/PUT/Delete to Read/Update/Delete lcs
+if you are an employee of the applicant, issuing bank, or beneficiary
+# DeLETE, if
+- you are the applicant or issuing bank, and the LC is not (beneficiary_approved && issuer_approved)
 
+3. /lc/{lc_id}/approve
+# POST to approve an LC during redlining, as either an employee of
+    (<lc for which id == lc_id>.issuer)
+or
+    (<lc for which id == lc_id>.beneficiary)
 
-TODO more
+4. /lc/{lc_id}/notify
+# POST the following
+{
+    'to_notify' : 'email_of_teammate@issuingbank.com',
+    'note' : 'optionally, send a note with the request'
+}
+# as a currently assigned BankEmployee of the issuing bank to notify a teammate of some need on the LC. If the teammate is not yet assigned to this LC, this will assign them to it.
+
+5. /lc/{lc_id}/doc_req
+# POST the following
+{
+    'submitted_doc' : [the submitted doc.pdf],
+    'for_doc_req' : optional, the int of the doc req this is for. this only applies to DigitalLCs
+}
+as an employee of the beneficiary to submit a DocumentaryRequirement
+    if its for a PdfLC we'll be creating the DocumentaryRequirement
+    if its for a DigitalLC we'll be updating the DocumentaryRequirement
+or, POST the following
+{
+    'approve': true || false,
+    'complaints' : 'any complaints; blank if approve == true'
+}
+as an employee of the issuing bank or client to approve/dispute a DocumentaryRequirement
+
+6. /lc/{lc_id}/doc_req/{doc_req_id}
+# GET a doc req, whether or not a doc has been submitted yet
+
+7. /lc/{lc_id}/request
+# POST as an employee of the beneficiary to request payment
+
+8. /lc/{lc_id}/draw
+# POST as an employee of the beneficiary to demand a draw on the LC
+
+9. /lc/{lc_id}/payout
+# POST as an employee of the client or bank to mark an LC as paid out
+
 """
 
 urlpatterns = [
