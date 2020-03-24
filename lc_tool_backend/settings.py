d5e5162e6b1e956ce75db639f634f14da9f3b102
Steve Flanagan <flanagan.s@husky.neu.edu>
2020-03-23 22:18:30 -0400

    added credential importing and settings to enable send_mail

--- lc_tool_backend/settings.py
+++ lc_tool_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from credentials import email_host_password
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,6 +28,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'steve@bountium.org'
+EMAIL_HOST_PASSWORD = email_host_password
 
 # Application definition
 
